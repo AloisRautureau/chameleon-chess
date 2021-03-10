4671548d3ea64ef6ee2c15afba192fa28deb841c
Alois <alois.rautureau@gmail.com>
2021-03-10 17:12:47 +0100

    Regenerated CMakeFiles

---
+++ CMakeFiles/baub_chess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baub_chess.dir/main.cpp.o"
+  "baub_chess"
+  "baub_chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/baub_chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
